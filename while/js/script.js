8b7f2f5dc587f8b56e8627eef6ac181d82d739cb
Alfredodaniello <alfredo_daniello@libero.it>
2024-03-13 14:22:04 +0100

    cicle while completed

---
+++ while/js/script.js
@@ -0,0 +1,34 @@
+/*
+Data una lista della spesa:
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
+Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
+Svolgete questo esercizio in 2 versioni: col ciclo for e col ciclo while.
+Consigli:
+- Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+- Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while 
+*/
+
+alert("Ecco la tua lista della spesa!!!");
+
+//ottengo il mio array
+const list = ['Latte','Cacao','Insalata','Carote','Pomodori','Piatti pronti'];
+
+//seleziono il mio div 
+const container = document.querySelector("#list");
+
+//dichiaro la mia variabile fuori dal ciclo while e poi lo faccio partire
+let i = 0 
+while (i < list.length) {
+    let elementsList = list[i];
+    const divList = document.createElement("div");
+    container.append(divList);
+    divList.append(elementsList);
+    i++;
+}
